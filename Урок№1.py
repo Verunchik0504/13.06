b77780622ce23cb1d5095ca1f7e7f745995bd30d
Veronika <vernigoraveronika8@gmail.com>
2023-07-14 13:56:44 +1000

    initial

---
+++ Урок№1.py
@@ -0,0 +1,22 @@
+# O (N**2)
+'''
+def strcounter(s):
+    for sym in set(s):
+        counter = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                counter += 1
+        print(f'{sym} - {counter}')'''
+
+
+# O (N)
+def strcounter(s):
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+    
+    for sym, count in syms_counter.items():
+        print(f'{sym} - {count}')
+
+
+strcounter('zzzzzxcv')
